e0c3aef53d3e17dfdc1bfd758fe22d1022480a92
enz <enzo_naguiat@yahoo.com>
2019-01-22 16:03:14 +0800

    add views and link to company login and registration page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def company_home
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :companies
   devise_for :users
-  root to: 'static_pages#home'
-  get '/about', to: 'static_pages#about'
+  root              to: 'static_pages#home'
+  get '/about',     to: 'static_pages#about'
+  get '/company',   to: 'static_pages#company_home'
 end
