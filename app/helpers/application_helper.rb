960efbaad2c97457ba430a6398d3e5a86febd0b1
enz <enzo_naguiat@yahoo.com>
2019-01-30 16:18:25 +0800

    create login_helper for login/logout/register buttons that change depending on the login status of the user. then implement it on navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def login_helper
+    if current_user
+      (tag.li(link_to "Logout", destroy_user_session_path, method: :delete))
+    else
+      (tag.li(link_to "Login", new_user_session_path)) + " ".html_safe +
+      (tag.li(link_to "Register,", new_user_registration_path))
+    end
+  end
 end
