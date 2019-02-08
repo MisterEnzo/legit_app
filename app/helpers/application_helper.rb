3f5b54a4d2fcda426e055feaeb04a78a2160d6ea
enz <enzo_naguiat@yahoo.com>
2019-02-09 01:07:37 +0800

    add rating to companies, and implement rating display in company show view by creating and using a rating_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,28 @@ module ApplicationHelper
       (tag.li(link_to "Register", new_user_registration_path))
     end
   end
+
+  def rating_helper
+    #display number of full stars
+    rating = @company.rating
+    stars = (tag.span "") 
+    rating.to_i.times do
+      stars += (tag.i "star", class: "material-icons")
+    end
+    #display half star
+    decimal = rating - rating.to_i
+    if decimal >= 0.5
+      stars += (tag.i "star_half", class: "material-icons")
+      #display number of empty stars
+      (4 - rating.to_i).times do
+        stars += (tag.i "star_border", class: "material-icons")
+      end
+    else
+      #display number of empty stars
+      (5 - rating.to_i).times do
+        stars += (tag.i "star_border", class: "material-icons")
+      end
+    end
+    stars
+  end
 end
