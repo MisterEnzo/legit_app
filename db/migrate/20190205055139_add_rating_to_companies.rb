db306ece8bb5b2c4decc96b01cfa2855d085e781
enz <enzo_naguiat@yahoo.com>
2019-02-09 11:36:05 +0800

    remove default value for rating, make rating show rating as  'none yet' when the company hasn't been reviewed

--- db/migrate/20190205055139_add_rating_to_companies.rb
+++ db/migrate/20190205055139_add_rating_to_companies.rb
@@ -1,5 +1,5 @@
 class AddRatingToCompanies < ActiveRecord::Migration[5.2]
   def change
-    add_column :companies, :rating, :float, :default => 1
+    add_column :companies, :rating, :float
   end
 end
