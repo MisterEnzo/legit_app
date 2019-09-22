cfec856579ba244de6b9c86399a8e84e254a1d7e
enz <enzo_naguiat@yahoo.com>
2019-09-22 18:04:22 +0900

    setup profile page and add link to profile page in navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def login_helper
     if current_user
+      (tag.li(link_to "User Profile", edit_user_registration_path)) +
       (tag.li(link_to "Logout", destroy_user_session_path, method: :delete))
     else
       (tag.li(link_to "Login", new_user_session_path)) + " ".html_safe +
