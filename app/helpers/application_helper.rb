aa05a54156105892c45e280734b361c8969b6268
enz <enzo_naguiat@yahoo.com>
2019-09-30 23:33:46 +0900

    modify nav login_helper to reflect company session status and enable logging out of company

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,11 @@ module ApplicationHelper
     if current_user
       (tag.li(link_to "User Profile", edit_user_registration_path)) +
       (tag.li(link_to "Logout", destroy_user_session_path, method: :delete))
+    elsif current_company
+      (tag.li(link_to "Company Profile", edit_company_registration_path)) + " ".html_safe +
+      (tag.li(link_to "Logout", destroy_company_session_path, method: :delete))
     else
+      (tag.li(link_to "Company", "/company")) + " ".html_safe +
       (tag.li(link_to "Login", new_user_session_path)) + " ".html_safe +
       (tag.li(link_to "Register", new_user_registration_path))
     end
