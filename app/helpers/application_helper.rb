c817ad0ad0160fe48b6cb32937d5017bea42ef98
enz <enzo_naguiat@yahoo.com>
2019-11-01 22:29:17 +0900

    remove company home static page. modify the action text in user and company login/registration. update routes. modify nav bar so that company button immediately leads to company login page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
       (tag.li(link_to "Company Profile", company_path(current_company))) + " ".html_safe +
       (tag.li(link_to "Logout", destroy_company_session_path, method: :delete))
     else
-      (tag.li(link_to "Company", "/company")) + " ".html_safe +
+      (tag.li(link_to "Company", new_company_session_path)) + " ".html_safe +
       (tag.li(link_to "Login", new_user_session_path)) + " ".html_safe +
       (tag.li(link_to "Register", new_user_registration_path))
     end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
 
   root              to: 'static_pages#home'
   get '/about',     to: 'static_pages#about'
-  get '/company',   to: 'static_pages#company_home'
+  
 end
